ce2981d6d877d26fbf5b5262c83732171a12fc45
Sebas <josuensd@gmail.com>
2024-05-29 19:32:55 -0500

    Taller 13: Propiedades Reactivas

--- js/app.js
+++ js/app.js
@@ -1,6 +1,13 @@
 /*Archivo de configuracion de la aplicacion*/
 console.log(Vue);
 
+const estudiantes = [{ nombre: "Josue", apellido: "Ocapana" },
+{ nombre: "Test1", apellido: "Test1" },
+{ nombre: "Test2", apellido: "Test2" },
+{ nombre: "Test3", apellido: "Test3" },
+{ nombre: "Test4", apellido: "Test4" }
+];
+
 const app = Vue.createApp({
     /*Vue trabaja a traves de opciones con funcionalidades definidas
     Conocidas como Options API, que es similar a declarar metodos, pero
@@ -8,15 +15,61 @@ const app = Vue.createApp({
     para declarar o programar funcionalidad en JS usa la programacion declarativa que 
     se hace atraves de {}, donde se pone el codigo de JS, dentro de las {}
     se pone codigo JS*/
-   template: `
-    <h1> Hola mundo desde Vue.JS</h1>
-    <p>
-    {{1+1}}
-    </p>
-    <p>{{[1,2,3,4]}}</p>
-    <p>{{true ? 'verdadero':'False'}}</p>
 
-    `
+    /*Un beneficio es poder insertar cualquier sentencia JS en una declaracion con ()
+    Como tal esto no es buena practica, pues JS es para toda la logica 
+    que permite manejar los componentes visuales, por ejemplo al hacer u 
+    formulario y crear un objeto con atributos, por ello en Js se programa toda la LOGICA 
+    DE PRESETACION (Logica para que la pag web funcione), en esta se puede incluir 
+    elementos HTML pero no es una buena practica.
+    Por esto surge el patron de diseño MVC, el mas simple es la vista, modelo(conjunto de datos q maneja la vista, elementos que se manejan "objeto que engloba la vista")  
+    controlador (Controla todas las peticiones de la vista), 
+    */
+    /*template: `
+     <h1> Hola mundo desde Vue.JS</h1>
+     <p>
+     {{1+1}}
+     </p>
+     <p>{{[1,2,3,4]}}</p>
+     <p>{{true ? 'verdadero':'False'}}</p>
+ 
+     `*/
+
+    /**propiedades reactivas */
+    data() {
+        return {
+            /**Declara un atributo simple */
+            mensaje: "Hola mundo propiedad reactiva",
+            valor: 7,
+            lista: estudiantes
+        }
+    },
+
+    /*Metodos
+    Cada funcionalidad va dentro de un methods*/
+
+    methods: {
+        /*Toda propiedad reactiva que va dentro de un archivo JS
+        Va con la palabra this */
+        cambiarTexto() {
+            this.mensaje = "Texto Cambiado";
+        },
+
+        sumar() {
+            this.valor = this.valor + 100;
+        },
+
+        agregar(){
+            const nuevo = {
+                nombre:"Josu ******",
+                apellido:"Josue ******"
+            }
+
+            this.lista.unshift(nuevo);
+        }
+
+    }
+
 });
 
 app.mount("#myApp");
